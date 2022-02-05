8d00fd62fa957bc48654d08092287a6fd3ea2c65
govvalamahesh <95411583+govvalamahesh@users.noreply.github.com>
2022-02-05 12:48:30 +0530

    Adding Project1

---
+++ src/main/java/com/gmk/Warrior/SimpleIntrest.java
@@ -0,0 +1,17 @@
+package com.gmk.Warrior;
+
+public class SimpleIntrest {
+
+	public float findSimpleIntrest(float principalAmount, float  rateOfIntrest, float time)
+	{
+		float SI = (principalAmount*rateOfIntrest*time)/100;
+		return SI;
+	}
+	
+	public static void main(String[] args)
+	{
+		SimpleIntrest si = new SimpleIntrest();
+		float f= si.findSimpleIntrest(37000, 2, 7);
+		System.out.println("SImple Intrest"+f);
+	}
+}
---
+++ src/main/java/com/gmk/Warrior/StudentMarksCalaculate.java
@@ -0,0 +1,27 @@
+package com.gmk.Warrior;
+
+import java.util.Scanner;
+
+public class StudentMarksCalaculate {
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		try {
+		int n,percentage = 0,total = 0;
+		
+		n= sc.nextInt();
+		for(int i=1;i<n;i++)
+		{
+			
+		}
+		
+		System.out.println("Sum of All marks"+total);
+		System.out.println("Percentage"+percentage);
+	}
+	catch(Exception e)
+	{
+	sc.close();	
+	}
+   }
+}
